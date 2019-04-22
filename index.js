c9e89d088363d3d12f5a0c6c2250db4bf14c29ea
Alexander Myers <alexander.j.myers@icloud.com>
2019-04-22 21:13:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  return document.querySelector(".target")
+}
+
+function deepestChild() {
+  var x = document.querySelector("#grand-node")
+  console.log(x.length);
+  return x
+
+}
+
+function increaseRankBy(n) {
+
+}
